306a2e4132c69ac9279d0b34911399503e0e7966
pedrovbo <pedrovicboliveira@gmail.com>
2021-10-25 22:55:24 -0300

    add some old studies

--- PHP/instanciando_objeto_fruta.php
+++ PHP/instanciando_objeto_fruta.php
@@ -5,4 +5,5 @@ $fruta1->setNome("morango");
 $fruta1->setTipo("vermelha");
 $fruta2 = new Fruta();
 $fruta2->setNome("laranja");
-$fruta2->setTipo("cítrica");
+$fruta2->setTipo("cítrica"); 
+// Testar depois de configurar o ambiente PHP 
