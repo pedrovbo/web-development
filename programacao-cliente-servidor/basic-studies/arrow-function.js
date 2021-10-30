edf159c2320e94574a73a88f819d387443383854
Pedro Oliveira <pedrovicboliveira@gmail.com>
2021-10-29 23:46:26 -0300

    add arrow-function-map

--- programacao-cliente-servidor/basic-studies/arrow-function.js
+++ programacao-cliente-servidor/basic-studies/arrow-function.js
@@ -1,4 +1,10 @@
 const multiplicacao = (n1, n2) => {
     return n1 * n2;
 }
-console.log(multiplicacao(3, 2));
+console.log("Resultado da primeira arrow function: " + multiplicacao(3, 2));
+
+// ou ainda mais simples
+
+
+const multiplicacao2 = (n1, n2) => n1 * n2;
+console.log("Resultado da segunda arrow function: " + multiplicacao2(3, 5));
